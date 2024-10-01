ef6b3b2df6ee408aedd8380000e841736107cc53
Sandra <oyarekhuas@gmail.com>
2024-10-01 12:07:12 +0100

    Added Render URL

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -4,7 +4,7 @@ export const getApiUrl = () => {
     return "http://localhost:5000";
   }
   // Replace this with your actual Render backend URL
-  return "https://hosting-jjjg.onrender.com";
+  return "https://voterz-pyg4.onrender.com";
 };
 
 export const API_URL = getApiUrl();
